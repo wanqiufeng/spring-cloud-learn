4fe65c1c89994e91ab380d6eb30ca1fea9b99192
unknown <wanqiufeng@outlook.com>
2020-09-22 22:00:50 +0800

    first commit

---
+++ src/test/java/com/vincent/springcloudlearn/SpringCloudLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.springcloudlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
